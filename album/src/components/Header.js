f9633712ad8813dd3aa819c11fd6a22998af01a4
Pat <cookepa1@gmail.com>
2018-11-07 09:55:49 -0500

    created project, completed through lecture 28
    completed implimentation of the header

---
+++ album/src/components/Header.js
@@ -0,0 +1,37 @@
+// import libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// make a component
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    
+    return (
+      <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+      </View>
+    );
+};
+
+const styles = {
+  textStyle: {
+      fontSize: 20
+  },
+  viewStyle: {
+      backgroundColor: '#F8F8F8',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 60,
+      paddingTop: 15,
+      //Adding a shadow below the header as a boarder instead of a solid line
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      // no explaination of this as of yet, but will be updated later
+      elevation: 2,
+      position: 'relative'
+  }
+};
+
+// make the component available to other parts of the app
+export default Header;

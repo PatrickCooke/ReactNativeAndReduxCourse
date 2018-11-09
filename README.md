cb4bcae2e52a2cd36788d7dfb7d04a3ef907ef3a
Pat <cookepa1@gmail.com>
2018-11-09 10:46:55 -0500

    completed 54
    finished first app
    button is reusable and can launch native web browser

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ReactNativeAndReduxCourse
+
+Learning React Native and Redux through a course available at https://www.udemy.com/the-complete-react-native-and-redux-course
--- album/src/components/AlbumDetail.js
+++ album/src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const { 
     imageContainerStyle,
     headerContentStyle,
@@ -22,16 +23,24 @@ const AlbumDetail = ({ album }) => {
           source={{ uri: thumbnail_image }} 
           />
         </View>
+
         <View style={headerContentStyle}>
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
         </CardSection>
+
         <CardSection>
           <Image 
           style={imageContainerStyle}
           source={{ uri: image }}
           />
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={() => Linking.openURL(url)} >
+          Buy Now
+          </Button>
         </CardSection>
     </Card>
   );

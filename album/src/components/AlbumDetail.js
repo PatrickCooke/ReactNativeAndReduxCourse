f66c0aea7956333562e28de7bb53c019950c3c9c
Pat <cookepa1@gmail.com>
2018-11-09 09:12:59 -0500

    completed 48
    added album image and styling
    made entire albumlist scrollable

--- album/src/components/AlbumDetail.js
+++ album/src/components/AlbumDetail.js
@@ -4,35 +4,61 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const { 
+    imageContainerStyle,
+    headerContentStyle,
+    headerTextStyle,
+    thumbnailContainerStyle,
+    thumbnailStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image 
           style={thumbnailStyle}
           source={{ uri: thumbnail_image }} 
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+        </CardSection>
+        <CardSection>
+          <Image 
+          style={imageContainerStyle}
+          source={{ uri: image }}
+          />
         </CardSection>
     </Card>
   );
 };
 
-const styles= {
+const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    marginRight: 10,
+    marginLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imageContainerStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 

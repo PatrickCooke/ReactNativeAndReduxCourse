5ad6db3dcea947bf23478689421ddb6ba1d55645
Pat <cookepa1@gmail.com>
2018-11-13 09:51:45 -0500

    Completed 59
    Registered/Added Firebase init to Auth app

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBQD80iZT_n7ILuSAdz1eSccsHTSUo3Oz0',
+      authDomain: 'auth-328e2.firebaseapp.com',
+      databaseURL: 'https://auth-328e2.firebaseio.com',
+      projectId: 'auth-328e2',
+      storageBucket: 'auth-328e2.appspot.com',
+      messagingSenderId: '696798778342'
+    });
+  }
+
   render() {
     return (
       <View>

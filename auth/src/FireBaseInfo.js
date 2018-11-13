12b199afce9f821b6ac6213805a4132d4e4b6eca
Pat <cookepa1@gmail.com>
2018-11-13 13:49:07 -0500

    updated FireBaseInfo to fix problems

---
+++ auth/src/FireBaseInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FireBaseInfo = () => {
+  var apiKey = '';
+  var authDomain = '';
+  var databaseURL = '';
+  var projectId = 'auth-d8d45';
+  var storageBucket = '';
+  var messagingSenderId = '';
+}
+
+export default FireBaseInfo

d48056a7fed0b508a8e6fae7529ef9502dd04455
Pat <cookepa1@gmail.com>
2018-12-18 15:27:01 -0500

    Completed lesson 126
    also added router-flux

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 class LoginForm extends Component {
@@ -31,6 +31,18 @@ class LoginForm extends Component {
     }
   }
 
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    }
+
+    return (
+    <Button onPress={this.onButtonPress.bind(this)}>
+      Login
+    </Button>
+    );
+  }
+
   render() {
     return (
       <Card>
@@ -56,10 +68,7 @@ class LoginForm extends Component {
         {this.renderError()}
 
         <CardSection>
-          <Button 
-          onPress={this.onButtonPress.bind(this)}>
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
@@ -78,7 +87,8 @@ const mapStateToProps = state => {
   return {
     email: state.auth.email,
     password: state.auth.password,
-    error: state.auth.error
+    error: state.auth.error,
+    loading: state.auth.loading
   };
 };
 
